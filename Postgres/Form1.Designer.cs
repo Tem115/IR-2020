4c6033b2a6c7e400c791c15d4d0c5ee3cffb83f6
Tem115 <37000158+Tem115@users.noreply.github.com>
2020-04-03 18:44:47 +0500

    2020

---
+++ Postgres/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Postgres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name_Film = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Year = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(47, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(386, 36);
+            this.textBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Name_Film,
+            this.Year});
+            this.dataGridView1.Location = new System.Drawing.Point(47, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(386, 301);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(475, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ПОиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Name_Film
+            // 
+            this.Name_Film.HeaderText = "Name_Film";
+            this.Name_Film.MinimumWidth = 6;
+            this.Name_Film.Name = "Name_Film";
+            this.Name_Film.ReadOnly = true;
+            // 
+            // Year
+            // 
+            this.Year.HeaderText = "Year";
+            this.Year.MinimumWidth = 6;
+            this.Year.Name = "Year";
+            this.Year.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 436);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ПОисковик";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name_Film;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Year;
+    }
+}
+
---
+++ Postgres/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Postgres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 1;
+            if (textBox1.Text == "")
+                return;
+            string[] words = textBox1.Text.Split(new char[] { ' ' });
+
+            string myConnectionString = "Server=localhost;Port=5432;User Id=postgres;Password=Qwerty12;Database=Mirvoda;";
+            string queryString = "SELECT * FROM movies WHERE ";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (int.TryParse(words[i], out int year))
+                {
+                    queryString = queryString + "(strpos(name, '" + words[i] + "') > 0 OR year = " + year + ")";
+                }
+                else
+                    queryString = queryString + "(strpos(name, '" + words[i] + "') > 0)";
+
+                if (i != words.Length - 1)
+                    queryString = queryString + " AND ";
+            }
+
+
+            NpgsqlConnection myConnection = new NpgsqlConnection(myConnectionString);
+            myConnection.Open(); //Открываем соединение.
+
+
+            NpgsqlCommand COM = new NpgsqlCommand(queryString, myConnection);
+            NpgsqlDataReader DR = COM.ExecuteReader();
+
+
+            var rowIndex = 0;
+            while (DR.Read())
+            {
+                dataGridView1.Rows.Add();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    dataGridView1.Rows[rowIndex].Cells[i].Value = DR.GetValue(i);
+                }
+                rowIndex++;
+                if (rowIndex > 10)
+                {
+                    MessageBox.Show(
+"Поиск завершен",
+"Предупреждение",
+MessageBoxButtons.OK,
+MessageBoxIcon.Information,
+MessageBoxDefaultButton.Button1,
+MessageBoxOptions.DefaultDesktopOnly);
+                    return;
+                }
+            }
+            myConnection.Close();
+            MessageBox.Show(
+"Поиск завершен",
+"Предупреждение",
+MessageBoxButtons.OK,
+MessageBoxIcon.Information,
+MessageBoxDefaultButton.Button1,
+MessageBoxOptions.DefaultDesktopOnly);
+        }
+    }
+}
